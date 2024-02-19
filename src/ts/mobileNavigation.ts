5ed41ff31a1147795a6cf04d0bd4a945003c725b
Bedkow <bednarekdaniel@o2.pl>
2024-02-19 16:23:05 +0100

    more styles focus and tab navigation modal added

--- src/ts/mobileNavigation.ts
+++ src/ts/mobileNavigation.ts
@@ -1,12 +1,48 @@
-const menuTrigger: any = document.querySelector("#menu__trigger");
-const mobileNav: any = document.querySelector("#menu__nav--mobile");
+const menuTrigger: HTMLElement = document.querySelector("#menu__trigger")!;
+const mobileNav: HTMLElement = document.querySelector("#menu__nav--mobile")!;
+const CTAs: NodeListOf<Element> = document.querySelectorAll(".CTA");
 
 mobileNav.classList.add("hidden");
 
-menuTrigger?.addEventListener("click", async (e: any) => {
-	console.log("menu trigger clicked");
-	e.currentTarget?.classList.toggle("active");
+menuTrigger?.addEventListener("click", () => {
+	menuTrigger?.classList.toggle("active");
 	mobileNav?.classList.toggle("active");
+	if (mobileNav?.classList.contains("active")) {
+		CTAs.forEach((el: Element) => {
+			el.setAttribute("tabindex", "-1");
+		});
+	} else {
+		CTAs.forEach((el: Element) => {
+			el.setAttribute("tabindex", "0");
+		});
+	}
+
+	if (mobileNav.classList.contains("hidden")) {
+		mobileNav.classList.remove("hidden");
+	} else {
+		// prevent clicking on hamburger during animation
+		menuTrigger.style.pointerEvents = "none";
+		setTimeout(() => {
+			mobileNav.classList.add("hidden");
+			//allow clicking on hamburger
+			menuTrigger.style.pointerEvents = "auto";
+		}, 500);
+	}
+});
+
+menuTrigger?.addEventListener("keypress", (e: any) => {
+	menuTrigger?.classList.toggle("active");
+	mobileNav?.classList.toggle("active");
+
+	if (mobileNav?.classList.contains("active")) {
+		CTAs.forEach((el: Element) => {
+			el.setAttribute("tabindex", "-1");
+		});
+	} else {
+		CTAs.forEach((el: Element) => {
+			el.setAttribute("tabindex", "0");
+		});
+	}
 
 	if (mobileNav.classList.contains("hidden")) {
 		mobileNav.classList.remove("hidden");
--- src/ts/registerInterest.ts
+++ src/ts/registerInterest.ts
@@ -1,20 +1,30 @@
-const registerInterestBtn: Element = document.querySelector(
+const registerInterestBtn: HTMLElement = document.querySelector(
 	"#register-interest-btn"
 )!;
-const closeModalBtn: Element = document.querySelector("#close-modal")!;
-const modalBlur: Element = document.querySelector("#modal-blur")!;
-const body: Element = document.querySelector("body")!;
-const header: Element = document.querySelector("header")!;
-const main: Element = document.querySelector("main")!;
+const closeModalBtn: HTMLElement = document.querySelector("#close-modal")!;
+const modalBlur: HTMLElement = document.querySelector("#modal-blur")!;
+const modal: any = document.querySelector("#modal")!;
+const header: HTMLElement = document.querySelector("header")!;
+const main: HTMLElement = document.querySelector("main")!;
+const nameInput: HTMLElement = document.querySelector("#name-input")!;
+const nameContainer: HTMLElement = document.querySelector(
+	"#modal-header__name-container"
+)!;
 
 registerInterestBtn.addEventListener("click", () => {
 	header.classList.toggle("blurred");
 	main.classList.toggle("blurred");
 	modalBlur.classList.toggle("modal-active");
+	modal.showModal();
 });
 
 closeModalBtn.addEventListener("click", () => {
 	header.classList.toggle("blurred");
 	main.classList.toggle("blurred");
 	modalBlur.classList.toggle("modal-active");
+	modal.close();
+});
+
+nameInput.addEventListener("input", (e: any) => {
+	nameContainer.textContent = e.target.value;
 });
