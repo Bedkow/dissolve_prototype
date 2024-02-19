f1829657c3fbfc3fc936fa020f72503410d09f6c
Bedkow <bednarekdaniel@o2.pl>
2024-02-19 20:43:53 +0100

    resolve overflow issues, rework mobile nav, adjust styles

--- src/ts/mobileNavigation.ts
+++ src/ts/mobileNavigation.ts
@@ -1,58 +1,49 @@
 const menuTrigger: HTMLElement = document.querySelector("#menu__trigger")!;
 const mobileNav: HTMLElement = document.querySelector("#menu__nav--mobile")!;
 const CTAs: NodeListOf<Element> = document.querySelectorAll(".CTA");
+const navLinksMobile: NodeListOf<Element> =
+	document.querySelectorAll(".nav-link--mobile");
 
 mobileNav.classList.add("hidden");
+mobileNav.style.visibility = "hidden";
+
+const toggleMobileMenu = () => {
+	menuTrigger?.classList.toggle("active");
+	mobileNav?.classList.toggle("active");
+	if (mobileNav?.classList.contains("active")) {
+		CTAs.forEach((el: Element) => {
+			el.setAttribute("tabindex", "-1");
+		});
+		navLinksMobile.forEach((el: Element) => {
+			el.setAttribute("tabindex", "0");
+		});
+	} else {
+		CTAs.forEach((el: Element) => {
+			el.setAttribute("tabindex", "0");
+		});
+		navLinksMobile.forEach((el: Element) => {
+			el.setAttribute("tabindex", "-1");
+		});
+	}
+
+	if ((mobileNav.style.visibility = "hidden")) {
+		mobileNav.style.visibility = "visible";
+	} else {
+		// prevent clicking on hamburger during animation
+		menuTrigger.style.pointerEvents = "none";
+		setTimeout(() => {
+			mobileNav.style.visibility = "hidden";
+
+			//allow clicking on hamburger
+			menuTrigger.style.pointerEvents = "auto";
+		}, 500);
+	}
+};
 
 menuTrigger?.addEventListener("click", () => {
-	menuTrigger?.classList.toggle("active");
-	mobileNav?.classList.toggle("active");
-	if (mobileNav?.classList.contains("active")) {
-		CTAs.forEach((el: Element) => {
-			el.setAttribute("tabindex", "-1");
-		});
-	} else {
-		CTAs.forEach((el: Element) => {
-			el.setAttribute("tabindex", "0");
-		});
-	}
-
-	if (mobileNav.classList.contains("hidden")) {
-		mobileNav.classList.remove("hidden");
-	} else {
-		// prevent clicking on hamburger during animation
-		menuTrigger.style.pointerEvents = "none";
-		setTimeout(() => {
-			mobileNav.classList.add("hidden");
-			//allow clicking on hamburger
-			menuTrigger.style.pointerEvents = "auto";
-		}, 500);
-	}
+	toggleMobileMenu();
 });
 
-menuTrigger?.addEventListener("keypress", () => {
-	menuTrigger?.classList.toggle("active");
-	mobileNav?.classList.toggle("active");
-
-	if (mobileNav?.classList.contains("active")) {
-		CTAs.forEach((el: Element) => {
-			el.setAttribute("tabindex", "-1");
-		});
-	} else {
-		CTAs.forEach((el: Element) => {
-			el.setAttribute("tabindex", "0");
-		});
-	}
-
-	if (mobileNav.classList.contains("hidden")) {
-		mobileNav.classList.remove("hidden");
-	} else {
-		// prevent clicking on hamburger during animation
-		menuTrigger.style.pointerEvents = "none";
-		setTimeout(() => {
-			mobileNav.classList.add("hidden");
-			//allow clicking on hamburger
-			menuTrigger.style.pointerEvents = "auto";
-		}, 500);
-	}
+menuTrigger?.addEventListener("keypress", (e: KeyboardEvent) => {
+	e.key === "Enter" ? toggleMobileMenu() : null;
 });
